cdc2425afa68960615ad194010c598fa2ca125a5
Matt Mayfield <mdmayfield@yahoo.com>
2026-02-01 12:17:38 -0500

    feat: debug log control

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,13 +1,20 @@
 const TARGET_DESKTOPS = 6;
+const DEBUG_ENABLE = false;
 const oneDtMultiDispState = new Map();
 const multDtOneDispState = new Map();
 let savedCurrentDesktopIndex = 0;
+
+function debugLog(message) {
+    if (DEBUG_ENABLE) {
+        console.info(message);
+    }
+}
 
 function saveWindowState(stateMap) {
     stateMap.clear();
 
     workspace.windowList().forEach(win => {
-        console.info(`setting state for ${win.resourceName} ${win.resourceClass}`);
+        debugLog(`setting state for ${win.resourceName} ${win.resourceClass}`);
         stateMap.set(win, {
             desktops: [...win.desktops.map((winDesktop) => {
                 return workspace.desktops.findIndex((desktop) => {
@@ -24,20 +31,20 @@ function saveWindowState(stateMap) {
 
 function restoreWindowState(stateMap) {
     stateMap.forEach((state, win) => {
-        console.info(`restoring ${win.resourceName} ${win.resourceClass}`);
+        debugLog(`restoring ${win.resourceName} ${win.resourceClass}`);
         if (workspace.windowList().includes(win)) {
-            console.info(`Would restore desktops ${JSON.stringify(state.desktops)}`);
+            debugLog(`Would restore desktops ${JSON.stringify(state.desktops)}`);
             const targetDesktops = state.desktops.map((savedDesktopIndex) => {
-                console.info(`in map index ${savedDesktopIndex}`);
+                debugLog(`in map index ${savedDesktopIndex}`);
                 if (workspace.desktops.length > savedDesktopIndex &&
                     savedDesktopIndex >= 0
                 ) { return workspace.desktops[savedDesktopIndex]; } else {
                     return -1;
                 }
             }).filter(i => i !== -1);
-            console.info(`targetDesktops ${JSON.stringify(targetDesktops)}`);
+            debugLog(`targetDesktops ${JSON.stringify(targetDesktops)}`);
             if (targetDesktops.length > 0) {
-                console.info(`setting win.desktops to targetDesktops`);
+                debugLog(`setting win.desktops to targetDesktops`);
                 win.desktops = targetDesktops;
             }
             // OMG KWin is not well documented for this
@@ -53,7 +60,7 @@ function restoreWindowState(stateMap) {
             // ONLY NOW can we update the window's sizing/pos without silently failing
             win.frameGeometry = targetFrameGeometry;
         } else {
-            console.info('NOT IN WORKSPACE');
+            debugLog('NOT IN WORKSPACE');
         }
     });
 }
@@ -62,34 +69,34 @@ function toggleDesktops() {
     const current = workspace.desktops.length;
 
     if (current === 1) {
-        console.info('Switching 1->6');
+        debugLog('Switching 1->6');
         saveWindowState(oneDtMultiDispState);
         for (let i = current; i < TARGET_DESKTOPS; i++) {
             workspace.createDesktop(i, `Desktop ${i + 1}`);
         }
         workspace.desktopGridHeight = 2;
-        console.info('Map size:', multDtOneDispState.size);
+        debugLog('Map size:', multDtOneDispState.size);
         multDtOneDispState.forEach((state, win) => {
-            console.info(`Key: ${win.resourceName}, State:`, JSON.stringify(state));
+            debugLog(`Key: ${win.resourceName}, State:`, JSON.stringify(state));
         });
 
         restoreWindowState(multDtOneDispState);
-        console.info('trying to go to desktop index ' + savedCurrentDesktopIndex + `of length ${workspace.desktops.length}`);
+        debugLog('trying to go to desktop index ' + savedCurrentDesktopIndex + `of length ${workspace.desktops.length}`);
         if (workspace.desktops.length > savedCurrentDesktopIndex &&
             savedCurrentDesktopIndex >= 0
         ) {
             workspace.currentDesktop = workspace.desktops[savedCurrentDesktopIndex];
         }
     } else {
-        console.info('Switching 6->1');
+        debugLog('Switching 6->1');
         savedCurrentDesktopIndex = workspace.desktops.findIndex((desktop) => {
             return desktop.id === workspace.currentDesktop.id;
         });
-        console.info(`savedCurrentDesktopIndex ${savedCurrentDesktopIndex}`);
+        debugLog(`savedCurrentDesktopIndex ${savedCurrentDesktopIndex}`);
         saveWindowState(multDtOneDispState);
-        console.info('Map size:', multDtOneDispState.size);
+        debugLog('Map size:', multDtOneDispState.size);
         multDtOneDispState.forEach((state, win) => {
-            console.info(`Key: ${win.resourceName}, State:`, JSON.stringify(state));
+            debugLog(`Key: ${win.resourceName}, State:`, JSON.stringify(state));
         });
         for (let i = current - 1; i >= 1; i--) {
             workspace.removeDesktop(workspace.desktops[i]);
